042013deea0f7bafee40081ac58d9fc628a9ce2b
Daniel Karlsson <oumu@Daniels-MacBook-Pro.local>
2018-02-02 15:05:11 +0100

    Added file DBfunctions.py

---
+++ prototyping/DBfunctions.py
@@ -0,0 +1,23 @@
+from AlchemyDB.py import *
+
+def getSensorReading(timestamp, module, sensor):
+    #TODO
+        #Fetch sensor reading table
+        #Compare database timestamp with call timestmap
+        #return value
+    pass
+
+def getConnectedSensors(moduleName):
+    #TODO
+        #getModule(moduleName) find the module entry
+        #Get sensors from the return
+        #return sensor list
+    pass
+
+def getModule(moduleName):
+    #TODO
+    pass
+
+def getModules():
+    #TODO
+    pass
