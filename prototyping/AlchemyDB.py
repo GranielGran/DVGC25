6fc27e4660d31ec8b505f30995cbac498d7f7a73
Daniel Karlsson <oumu@Daniels-MacBook-Pro.local>
2018-02-08 13:49:50 +0100

    Changed some datatypes

--- prototyping/AlchemyDB.py
+++ prototyping/AlchemyDB.py
@@ -1,38 +1,38 @@
-from flask                          import Flask
-from sqlalchemy                     import Column, Date, Integer, String, ForeignKey
-from sqlalchemy                     import create_engine
-from sqlalchemy.ext.declarative     import declarative_base
-from sqlalchemy_utils               import database_exists, create_database
-from sqlalchemy.orm                 import relationship
-from datetime                       import datetime
-##########################################################################################
-#Config and Instansiation                                                                #
-##########################################################################################
+from flask import Flask
+from datetime import datetime
+from sqlalchemy import Column, Date, Integer, String, ForeignKey, DateTime, Float
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.orm import relationship
+
 
 engine = create_engine('sqlite:///database/datalog.db', echo=True)
+
+#If we can't find a the .db file, we create one at the given path
 if not database_exists(engine.url): create_database(engine.url) 
 
 Base = declarative_base()
 
-##########################################################################################
-#Tabel Creation                                                                         #
-##########################################################################################
-class Sensors(Base):
-    __tablename__   = 'sensors'
+'''
+Here we define the tables that are going into the database
+'''
+class Sensor(Base):
+    __tablename__   = 'Sensor'
     id              = Column(Integer, primary_key = True)
-    moduleID        = Column(Integer, ForeignKey('modules.id'), nullable = False)
-    reading         = Column(Integer, nullable = False)
-    timestamp       = Column(Integer, nullable = False, default = datetime.utcnow)
+    moduleID        = Column(Integer, ForeignKey('Module.id'), nullable = False)
+    reading         = Column(Float, nullable = False)
+    timestamp       = Column(DateTime, nullable = False, default = datetime)
     sensorName      = Column(String(10), nullable = False)
 
     def __repr__(self): 
         return '<Sensor %r>' % self.sensorName
 
-class Modules(Base):
-    __tablename__        = 'modules'
-    id                   = Column(Integer, primary_key = True)
-    moduleName           = Column(String(10), nullable = False)
-    sensorConnections    = relationship('Sensor')
+class Module(Base):
+    __tablename__   = 'Module'
+    id              = Column(Integer, primary_key = True)
+    moduleName      = Column(String(10), nullable = False)
+    Connections     = relationship('Sensor')
     
     def __repr__(self):
         return 'Module %r' % self.moduleName
